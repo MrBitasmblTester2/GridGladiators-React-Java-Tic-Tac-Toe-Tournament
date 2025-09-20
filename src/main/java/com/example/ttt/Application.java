f712c7db0aec23b8f95d60c3ce425fab9535d139
MrBitasmblTester2 <lulu2214@proton.me>
2025-09-20 22:07:09 +0400

    Add Application.java

---
+++ src/main/java/com/example/ttt/Application.java
@@ -0,0 +1,10 @@
+// src/main/java/com/example/ttt/Application.java
+package com.example.ttt;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
